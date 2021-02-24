11d6f4d8818df7b149084035955be5d0fc837eb5
sdrs6921 <sdrs6921@naver.com>
2021-02-24 17:22:06 +0900

    feature : init project

---
+++ src/main/java/com/dobby/movie_community/MovieCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.dobby.movie_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCommunityApplication.class, args);
+	}
+
+}
